2c67ac5d3a6c1a760b5ea25e2a926beee3187a92
Anthony Salemo <31021304+Ajsalemo@users.noreply.github.com>
2018-05-11 16:18:20 -0400

    protected routes

---
+++ client/src/utils/authenticateRoutes.js
@@ -0,0 +1,51 @@
+// Imports
+import React from "react";
+import PropTypes from "prop-types";
+import { Component } from "react";
+import { connect } from "react-redux";
+import { addFlashMessage } from "../actions/flashMessages";
+
+// Creating component wrapper
+export default function(ComposedComponent) {
+    class authenticateRoutes extends Component {
+        
+        componentWillMount() {
+            // If user isnt authenticated
+            if(!this.props.isAuthenticated) {
+                this.props.addFlashMessage({
+                    type: "error",
+                    text: "You need to login to access this page"
+                });
+                // Then redirect
+                this.context.router.history.push("/");
+            }
+        }
+
+        // Render the wrapper
+        render() {
+            return (
+                <ComposedComponent {...this.props}/>
+            )
+        }
+    }
+
+    // Setting propTypes
+    authenticateRoutes.propTypes = {
+        isAuthenticated: PropTypes.bool.isRequired,
+        addFlashMessage: PropTypes.func.isRequired
+    }
+
+    authenticateRoutes.contextTypes = {
+        router: PropTypes.object.isRequired
+    }
+
+    // Accessing redux store
+    function mapStateToProps(state) {
+        return {
+            isAuthenticated: state.auth.isAuthenticated
+        }
+    }
+
+    return connect(mapStateToProps, { addFlashMessage })(authenticateRoutes);
+}
+
