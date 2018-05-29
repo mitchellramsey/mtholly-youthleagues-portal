9706890f1f7b552cb2f9dfc3ac2081966b98dfc2
Anthony <ajssalemo@gmail.com>
2018-05-28 21:08:58 -0400

    test component

--- controllers/client-controllers.js
+++ controllers/client-controllers.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // ----------------------------------------------------------------------------------- //
 // Landing page
 router.get("*", function (req, res) {
-    res.send(File(path.join(__dirname, "./client/build/index.html")));
+    res.send(File(path.join(__dirname, "../client/public.html")));
 });
 
 // ----------------------------------------------------------------------------------- //
